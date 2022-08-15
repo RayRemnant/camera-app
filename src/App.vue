36fba8a5359c6fa79460af57d1bf44eff4eb9c98
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2022-08-15 22:08:42 +0000

    some progress, UI improvements next + api

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,90 @@
 <template>
   <div id="app">
-    <div id="qr-code-full-region"></div>
-    <div>
-      <button type="button" @click="message = []">clear</button>
+    <div id="reader"></div>
+    
+	<div :class="{show: newFlag}" class="resultView">
+	<div class="close-button">
+      <button type="button" @click="newFlag = false">X</button>
     </div>
-    <div
+		<hr/>
+      <pre>decodedResult => {{ message }}</pre>
+	</div>
+
+	<div class="resultHistory" style="display:none">
+		<div
       v-for="(msg, index) in message"
       :key="index"
       style="white-space: pre-line"
     >
-		<div>
-      <pre>decodedResult => {{ msg.decodedResult }}</pre>
-		</div>
+		
     </div>
+	</div>
   </div>
 </template>
 
 <script>
-import { Html5QrcodeScanner } from "html5-qrcode";
+import { Html5Qrcode } from "html5-qrcode";
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      message: [],
-	  newFlag
+      products: [],
+ newFlag: false
     };
   },
   methods: {
     creatScan() {
-      const config = { fps: 10, qrbox: 250 };
-      const html5QrcodeScanner = new Html5QrcodeScanner(
+		const qrBoxSize = 250
+ const width = window.innerWidth
+          const height = window.innerHeight
+          const aspectRatio = width / height
+          const reverseAspectRatio = height / width
+
+          const mobileAspectRatio = reverseAspectRatio > 1.5
+            ? reverseAspectRatio + (reverseAspectRatio * 12 / 100)
+            : reverseAspectRatio
+
+          const config = {
+            fps: 20, // frame per seconds for qr code scanning
+            qrbox: { width: qrBoxSize, height: qrBoxSize },
+            videoConstraints: {
+              facingMode: 'environment',
+              aspectRatio: width < 600
+                ? mobileAspectRatio
+                : aspectRatio,
+            },
+          }
+
+      /* const html5QrcodeScanner = new Html5QrcodeScanner(
         "qr-code-full-region",
         config
       );
-      html5QrcodeScanner.render(this.onScanSuccess);
-    },
-    onScanSuccess(decodedText, decodedResult) {
-      const obj = { decodedResult: decodedResult };
-      this.message.push(obj);
+	
+      html5QrcodeScanner.render(this.onScanSuccess); */
+
+ const html5QrCode = new Html5Qrcode(/* element id */ "reader");
+html5QrCode.start(
+	{ facingMode: "environment" },
+  config,
+  (decodedText, decodedResult) => {
+
+  this.newFlag = true;
+      this.products.push(decodedText);
+	console.log(decodedText, decodedResult)
+
+	axios
+      .get('https://world.openfoodfacts.org/api/v2/product/04963406')
+      .then(response => (this.info = response))
+  },
+  (errorMessage) => {
+	console.log(errorMessage)
+    // parse error, ignore it.
+  })
+.catch((err) => {
+	console.log(err)
+  // Start failed, handle it.
+});
     },
   },
   async mounted() {
